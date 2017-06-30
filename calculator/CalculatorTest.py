0fb94e2073610f2cb034bac9c22b160e328d38ed
Viet Code <vietcodecamp@gmail.com>
2017-06-30 11:46:31 +0200

    zadani

---
+++ calculator/CalculatorTest.py
@@ -0,0 +1,14 @@
+import unittest
+from calculator.Calculator import add, sub
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(5,3), 8)
+
+    def test_sub(self):
+        self.assertEqual(sub(5,3), 2)
+
+if __name__ == '__main__':
+    unittest.main()
