2d7862e0ba009ce68a123c8ea4d74b45379f27e0
NinjaOnRails <danny.hvu@gmail.com>
2017-06-30 18:08:15 +0200

    par ukolu pro 2. den

---
+++ ukoly/den2/collatz.py
@@ -0,0 +1,17 @@
+# Napis funkci collatz() s jednim parametrem: cislo. Pokud je cislo sude,
+# tak vytiskni cislo // 2 a return tuto hodnotu. Pokud je cislo liche, tak
+# collatz() by mel vytisknout a return 3 * cislo + 1.
+# Pote napis program ktery prijme jedno libovolne integer od uzivatele a
+# aplikuje funkci collatz() opakovane, az vrati hodnotu 1.
+
+# Priklad kdyz uzivatel zada 3, vysledek programu by mel vypadat takto:
+'''
+3
+10
+5
+16
+8
+4
+2
+1
+'''
---
+++ ukoly/den2/hezkyTisk.py
@@ -0,0 +1,16 @@
+# Napis funkci hezkyTisk() ktery prijma jeden argument seznam seznamu retezcu
+# a hezky ho vytiskne s metodou rjust() jako nize:
+
+'''
+
+  apples Alice  dogs
+ oranges   Bob  cats
+cherries Carol moose
+  banana David goose
+
+'''
+
+
+Data = [['jablka', 'pomerance', 'tresne', 'banan'],
+        ['Alenka', 'Honza', 'Karolina', 'David'],
+        ['pes', 'kocka', 'mys', 'husa']]
