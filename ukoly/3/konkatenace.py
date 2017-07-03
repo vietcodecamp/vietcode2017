f0b778c1f6097fad3d060cb698a400281a1056ef
NinjaOnRails <danny.hvu@gmail.com>
2017-07-03 14:41:55 +0200

    Pridany ukoly pro kapitolu 3.

---
+++ ukoly/3/konkatenace.py
@@ -0,0 +1,10 @@
+# Podobne jako nize, zkonkatenuj text a promenne ve dvou dalsich verzi
+# Jednu verzi s pouzitim % a druhou s pomoci {} a .format()
+
+druh = 'clovek'
+jmeno = 'Boromir'
+zbran = 'mec'
+mise = 'ochranit nositele prstenu moci'
+
+print('Jmenuji se ' + jmeno + ' a jsem ' + druh + '. Moji oblibenou zbrani se stal '
+      + zbran + ' a muj ukol je ' + mise + '.')
