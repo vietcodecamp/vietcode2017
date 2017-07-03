e05d870ca52c8ded68fcd90c5d6490e7b3994975
NinjaOnRails <danny.hvu@gmail.com>
2017-07-03 17:22:16 +0200

    Kapitola 5

--- ukoly/4/pridejDoSpam.py
+++
@@ -1,3 +0,0 @@
-# Nize do seznamu spam pridej hodnotu "parky" na 3. pozici
-
-spam = [2, 4, 6, 8, 10]
---
+++ ukoly/5/booleanPriklady.py
@@ -0,0 +1,8 @@
+# Jaky je vysledek kazdeho radku?
+
+(5 > 4) and (3 == 5)
+not (5 > 4)
+(5 > 4) or (3 == 5)
+not ((5 > 4) or (3 == 5))
+(True and True) and (True == False)
+(not False) or (not True)
