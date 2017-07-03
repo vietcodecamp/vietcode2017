955e6d43714846b237d0609cd316d67f561cfc6a
NinjaOnRails <danny.hvu@gmail.com>
2017-07-04 01:51:50 +0200

    ukoly

--- ukoly/5/aritmetika2.py
+++
@@ -1,5 +0,0 @@
-# 1.Vytiskni integer podil x//y
-# 2.Vytiskni float podil x/y
-
-x = int(input('x = '))
-y = int(input('y = '))
---
+++ ukoly/6/if_else.py
@@ -0,0 +1,6 @@
+# 1. Kdyz n je liche, vytiskni 'Divny'
+# 2. Kdyz n je sude a mezi 2 a 5, inclusive, vytiskni 'Normal'
+# 3. Kdyz n je sude a mezi 6 a 20, inclusive, vytiskni 'Divny'
+# 4. Kdyz n je sude a vetsi nez 20, vytiskni 'Normal'.
+
+n = int(input())
