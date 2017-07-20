463f68e19c0cfb59469140d047c82c49d4fee648
vietcodecamp <vietcodecamp@gmail.com>
2017-07-20 15:20:49 +0200

    bankomat

---
+++ ukoly/kapitola6/bankomat.py
@@ -0,0 +1,14 @@
+# Naprogramuj jednoduchy bankomat.
+# Pokud cislo karty se nebude rovnat 43-1199223344, tak vypis: "Neplatna karta! Vracim kartu!"
+# Pokud zada spravne cislo karty, tak se overi cislo pinu. Pokud se pin nebude rovnat 3194, tak vypis: "Neplatny PIN! Vracim kartu!"
+# Uzivatel si chce vybrat penize, ale ma na uctu jenom 1000 Kc. Pokud chce vybrat vic jak 1000, tak vypis: "Nedostatek penez na uctu! Vracim kartu!"
+# Pokud vsechno projde, tak vypis: "Vyplacim [castka] Kc"
+
+
+platna_karta = 43-1199223344
+pin = 3194
+zustatek_na_uctu = 1000
+
+def bankomat(cislo_karty :str, pin :int, castka :int):
+    # zde dopln svuj kod
+    pass
