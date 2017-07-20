adc2e1ee1d97d23d6100810954b219805058c88c
Nguyen Hoang Long <nguyeho7@fel.cvut.cz>
2017-07-20 14:31:02 +0200

    added function skeleton

--- ukoly/kapitola5/absolutni_hodnota.py
+++ ukoly/kapitola5/absolutni_hodnota.py
@@ -1,3 +1,6 @@
 # doplnte funkci, aby vzala vstup (cisla) od uzivatele
 # a nasledne vypsala jeji absolutni hodnotu
+# vypis "absolutni hodnota je [cislo]"
 
+def absolutni_hodnota():
+    # vas kod zde    
