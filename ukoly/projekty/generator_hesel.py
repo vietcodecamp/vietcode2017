dd4c143f5c0caa42fd58fed156fd4d4f46b9588b
Dacos Nguyen <dacos.nguyen@gmail.com>
2017-07-21 14:49:58 +0200

    Added 2 projects

---
+++ ukoly/projekty/generator_hesel.py
@@ -0,0 +1,9 @@
+"""
+Vytvor generator hesel, ktery bude generovat hesla dlouha minimalne 6 znaku.
+Uzivatel si muze urcit, aby se vygenerovalo desli heslo.
+Pozadavek je, aby heslo pozadovalo alespon 1 upper case znak, 1 lower case znak
+a alespon 1 specialni znak (!, @, $, &).
+Vyuzitej ASCII tabulku https://en.wikipedia.org/wiki/ASCII
+Vyuzijte funkci chr a vlozte decimalni reprezentaci pismena.
+chr(65) vrati A.
+"""
---
+++ ukoly/projekty/rozrazovac.py
@@ -0,0 +1,16 @@
+"""
+Mate k dispozici seznam lidi.
+Zeptejte se uzivatele do kolika skupin chcete rozradit lidi.
+Pokud mame napr. 14 lidi a uzivatel bude chtit rozradit lidi do 4 skupin,
+nastane problem v tom, ze 2 budou prebyvat. Tyto 2 lidi rozrad do ostatnich skupin.
+2 skupiny budou mit 3 lidi. 2 skupiny budou mit 4 lidi.
+Samozrejme musite napsat program tak, aby fungoval pro libovolny pocet lidi.
+Pokud uzivatel zada nesmyslne cislo napr. -5 nebo cislo vetsi nez pocet lidi,
+tak mu vypiste zpravu, aby opravil cislo.
+"""
+
+seznam_lidi = ["Lenka", "Thao Chi", "Hanka", "Tom", "Ondra", "Mila", "Jirka",
+               "Filip", "Linh Chi", "Tu", "Hung", "Honza", "Napoleon","Alex"]
+
+def rozrad():
+    pass
