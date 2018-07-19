e1b04f2719f06a0a00f429078b37e4410c1c3530
Dagy Tran <dagytran@gmail.com>
2018-07-19 09:38:39 +0200

    [Python Files] Lesson 2

---
+++ lecture_notes/Exercises/Lesson_2/ukladani_vstupu.py
@@ -0,0 +1,9 @@
+# Ukládání vstupu do listu
+# Napište program, který se zeptá na 3 jména, pak je uloží do seznamu a následně tato jména ze seznamu vypíše.
+
+
+jmena = [] # 1. vytvoření prázdného listu
+
+# 2.-4. získání jména ze vstupu a přidání ho do listu (Hint: funkce `append()`)
+
+# 5. Tisk odpovědi pomocí indexace
